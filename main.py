93a02ed7bfb9b49cbf32ef0df7807eb98cb201c0
casper_yue@163.com <casper_yue@163.com>
2019-02-28 22:19:28 +0800

    url forward function

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import qrcode
 from flask import Flask, make_response, request
 from lib.captcha.captcha import captcha
 from flask_restful import Resource, Api
@@ -8,9 +9,10 @@ api = Api(app)
 
 
 class Login(Resource):
-    def get(self):
+    # @staticmethod
+    def get(self, id):
         # TODO: need to return a login template
-        return {'hello': 'world'}
+        return {'hello': id}
 
     def post(self):
         # TODO:
@@ -32,6 +34,20 @@ class Verify(Resource):
         # TODO: need arguments uuid, code, mobile
         pass
 
+@app.route("/qrcode/")
+def generate_qrcode():
+    # url forward
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data('http:www.baidu.com')
+    qr.make(fit=True)
+    img = qr.make_image()
+    img.save("123.png")
+    return "hello"
 
 class Register(Resource):
     def get(self):
@@ -40,7 +56,7 @@ class Register(Resource):
     def post(self):
         pass
 
-api.add_resource(Login, '/login/')
+api.add_resource(Login, '/login/<id>')
 api.add_resource(Verify, '/verify/')
 api.add_resource(Register, '/register/')
 
