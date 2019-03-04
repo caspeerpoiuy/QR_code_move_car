4207b1c33425623f06117fadd546954d967e6bee
wangdiwangdi1992 <799895320@qq.com>
2019-03-04 23:12:33 +0800

    client

--- clientent_move_car/src/containers/register/register.js
+++ clientent_move_car/src/containers/register/register.js
@@ -8,14 +8,17 @@ import {
     Radio,
     Button
 } from 'antd-mobile'
+
+import {connect} from 'react-redux'
+import {Redirect} from 'react-router-dom'
 import Logo from '../../components/logo/logo'
+import {register} from '../../redux/actions'
 
 export default class Register extends Component{
     state = {
         username: '',
         password: '',
         password2: '',
-        type: 'dashen'
     }
 
     handleChange = (name, value) => {
@@ -37,32 +40,14 @@ export default class Register extends Component{
                 <Logo/>
                 <WingBlank>
                     <List>
-                        <InputItem
-                            placeholder='输入用户名'
-                            onChange={val => this.handleChange('username', val)}
-                        >
-                            用户名:
-                        </InputItem>
+                        <InputItem placeholder='输入用户名' onChange={val => this.handleChange('username', val)}>用户名:</InputItem>
                         <WhiteSpace/>
-                        <InputItem
-                            type='password'
-                            placeholder='输入密码'
-                            onChange={val => this.handleChange('password', val)}
-                        >
-                            密&nbsp;&nbsp;&nbsp;码:
-                        </InputItem>
+                        <InputItem type='password' placeholder='输入密码' onChange={val => this.handleChange('password', val)}>密&nbsp;&nbsp;&nbsp;码:</InputItem>
                         <WhiteSpace/>
-                        <InputItem
-                            type='password'
-                            placeholder='输入确认密码'
-                            onChange={val => this.handleChange('password2', val)}
-                        >
-                            确认密码:
-                        </InputItem>
+                        <InputItem type='password' placeholder='输入确认密码' onChange={val => this.handleChange('password2', val)}>确认密码:</InputItem>
                         <WhiteSpace/>
                         <WhiteSpace/>
-                        <Button type='primary' onClick={this.register}>注&nbsp;&nbsp;&nbsp;册
-                        </Button>
+                        <Button type='primary' onClick={this.register}>注&nbsp;&nbsp;&nbsp;册</Button>
                         <WhiteSpace/>
                         <Button onClick={this.toLogin}>已经有账号</Button>
                     </List>
