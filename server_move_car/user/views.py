e52649fe78f5f014ad17090bb21ce06877e2a37c
wangdiwangdi1992 <799895320@qq.com>
2019-03-09 17:27:36 +0800

    server

--- server_move_car/user/views.py
+++ server_move_car/user/views.py
@@ -21,3 +21,18 @@ def register(request):
     except Exception as e:
         print(e)
     return JsonResponse({"code":0,"data":"注册成功"})
+
+@csrf_exempt
+def login(request):
+    jsonloads = (json.loads(request.body.decode(encoding='utf-8')))
+    username = jsonloads['username']
+    password = jsonloads['password']
+    if not all([username,password]):
+        return JsonResponse({"code": 1, "msg": "数据不完整"})
+    isexist = User.objects.check_username(username)
+    if not isexist:
+        return JsonResponse({"code": 1, "msg": "用户名不存在"})
+    user = User.objects.get_one_user(username,password)
+    if not user:
+        return JsonResponse({"code": 1, "msg": "密码错误"})
+    return JsonResponse({"code": 0, "msg": "ok"})
